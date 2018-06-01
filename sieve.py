0d7a22e869bc2df257f2e598f10d917464b4cf48
loocash <lukaszkapica@gmail.com>
2018-06-01 14:02:45 +0200

    Refactor Code so that it is PEP8 compliant

--- sieve.py
+++ sieve.py
@@ -3,29 +3,30 @@
 from bisect import bisect_right
 from math import sqrt, floor
 
+
 class Sieve:
 
-  def first(self, k):
-    return self.primes[:k]
+    def first(self, k):
+        return self.primes[:k]
 
-  def upto(self, k):
-    index = bisect_right(self.primes, k)
-    return self.primes[:index]
+    def up_to(self, k):
+        index = bisect_right(self.primes, k)
+        return self.primes[:index]
 
-  def __init__(self, size):
-    assert(size > 1 and size < 2**30)
-    self.size = size
-    self._sieve()
-    self._make_primes()
+    def __init__(self, size):
+        assert (1 < size < 2 ** 30)
+        self.size = size
+        self._sieve()
+        self._make_primes()
 
-  def _sieve(self):
-    self.isPrime = [True for x in range(self.size+1)]
-    self.isPrime[0] = self.isPrime[1] = False
-    bound = int(floor(sqrt(self.size+1)))
-    for i in range(2, bound):
-      if self.isPrime[i]:
-        for j in range(i*i, self.size+1, i):
-          self.isPrime[j] = False
+    def _sieve(self):
+        self.isPrime = [True for _ in range(self.size + 1)]
+        self.isPrime[0] = self.isPrime[1] = False
+        bound = int(floor(sqrt(self.size + 1)))
+        for i in range(2, bound):
+            if self.isPrime[i]:
+                for j in range(i * i, self.size + 1, i):
+                    self.isPrime[j] = False
 
-  def _make_primes(self):
-    self.primes = list(filter(lambda x: self.isPrime[x], range(self.size+1)))
+    def _make_primes(self):
+        self.primes = list(filter(lambda x: self.isPrime[x], range(self.size + 1)))
--- test.py
+++ test.py
@@ -1,28 +1,32 @@
 import unittest
 from sieve import Sieve
 
+
 class TestSieve(unittest.TestCase):
-  def setUp(self):
-    self.sieve = Sieve(100)
+    def setUp(self):
+        self.sieve = Sieve(100)
 
-  def test_size(self):
-    self.assertEqual(self.sieve.size, 100)
+    def test_size(self):
+        self.assertEqual(self.sieve.size, 100)
 
-  def test_isPrime_83(self):
-    self.assertEqual(self.sieve.isPrime[83], True)
+    def test_isPrime_83(self):
+        self.assertEqual(self.sieve.isPrime[83], True)
 
-  def test_first_5_primes(self):
-    self.assertEqual(self.sieve.first(5), [2, 3, 5, 7, 11])
+    def test_first_5_primes(self):
+        self.assertEqual(self.sieve.first(5), [2, 3, 5, 7, 11])
 
-  def test_primes_upto_13(self):
-    self.assertEqual(self.sieve.upto(13), [2, 3, 5, 7, 11, 13])
+    def test_primes_up_to_13(self):
+        self.assertEqual(self.sieve.up_to(13), [2, 3, 5, 7, 11, 13])
 
-  def test_primes_upto_100(self):
-    self.assertEqual(self.sieve.upto(100), [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97])
+    def test_primes_up_to_100(self):
+        self.assertEqual(self.sieve.up_to(100),
+                         [2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89,
+                          97])
 
-  def test_primes_1000th_prime_is_7919(self):
-    s = Sieve(10000)
-    self.assertEqual(s.first(1000)[-1], 7919)
+    def test_primes_1000th_prime_is_7919(self):
+        s = Sieve(10000)
+        self.assertEqual(s.first(1000)[-1], 7919)
+
 
 if __name__ == '__main__':
-  unittest.main()
+    unittest.main()
